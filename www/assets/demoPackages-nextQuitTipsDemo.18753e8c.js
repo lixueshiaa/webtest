866a0ce9bbbfdd5e38b9744886d2bcf8a15068a2
lixueshi <920804994@qq.com>
2025-07-03 09:18:00 +0800

    update

---
+++ www/assets/demoPackages-nextQuitTipsDemo.18753e8c.js
@@ -0,0 +1,1 @@
+import{d as s,o,l as a,w as e,f as p,i as t}from"./index-59a1c9e4.js";import{o as n,a as l,b as i}from"./uni-app.es.92666eec.js";const u=s({__name:"nextQuitTipsDemo",setup:s=>(n((()=>{console.log("App Launch")})),l((()=>{console.log("App Show")})),i((()=>{console.log("App Hide")})),(s,n)=>{const l=t;return o(),a(l,null,{default:e((()=>[p("test")])),_:1})})});export{u as default};
